1c3f8c149487f9e4bbb0fa6685dfc6d774f96497
cat126 <cat1265@gmail.com>
2019-12-02 11:39:09 -0600

    Added More MetaData

--- JobBoard.DataLayer/Category.cs
+++ JobBoard.DataLayer/Category.cs
@@ -22,6 +22,7 @@ namespace JobBoard.DataLayer
     
         public int CategoryID { get; set; }
         public string CategoryDescription { get; set; }
+        public string CategoryName { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Position> Positions { get; set; }
--- JobBoard.DataLayer/JobBoardMetaData.cs
+++ JobBoard.DataLayer/JobBoardMetaData.cs
@@ -39,7 +39,69 @@ namespace JobBoard.DataLayer
     [MetadataType(typeof(ApplicationStatusMetaData))]
     public partial class ApplicationStatus { }
 
-    public partial class Category
+    public partial class CategoryMetaData
     {
+        [Required(ErrorMessage = "*")]
+        [StringLength(50, ErrorMessage = "value must be 50 character or less.")]
+        public string CategoryName { get; set; }
+        [StringLength(500, ErrorMessage = "value must be 500 character or less.")]
+        //TODO Bruce Smith add code to make this muit line
+        public string CategoryDescription { get; set; }
     }
- }
+    [MetadataType(typeof(CategoryMetaData))]
+    public partial class Category { }
+
+    public partial class LocationMetaData
+    {
+        [Required(ErrorMessage = "*")]
+        public string ManagerID { get; set; }
+        [Required(ErrorMessage = "*")]
+        public decimal Latitude { get; set; }
+        [Required(ErrorMessage = "*")]
+        public decimal Longitude { get; set; }
+        [Required(ErrorMessage = "*")]
+        [StringLength(1000, ErrorMessage = "value must be 1000 character or less.")]
+        public string Address { get; set; }
+    }
+    [MetadataType(typeof(LocationMetaData))]
+    public partial class Location { }
+
+    public partial class OpenPositionMetaData
+    {
+        [Required(ErrorMessage = "*")]
+        public int PositionID { get; set; }
+        [Required(ErrorMessage = "*")]
+        public int LocationID { get; set; }
+    }
+    [MetadataType(typeof(OpenPositionMetaData))]
+    public partial class OpenPosition { }
+
+    public partial class PositionMetaData
+    {
+        [Required(ErrorMessage = "*")]
+        [StringLength(50, ErrorMessage = "value must be 50 character or less.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "*")]
+        public string JobDescription { get; set; }
+        [Required(ErrorMessage = "*")]
+        public int CategoryID { get; set; }
+    }
+    [MetadataType(typeof(PositionMetaData))]
+    public partial class Position { }
+
+    public partial class UserDetailMetaData
+    {
+        [Required(ErrorMessage = "*")]
+        public string UserID { get; set; }
+        [Required(ErrorMessage = "*")]
+        [StringLength(50, ErrorMessage = "value must be 50 character or less.")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "*")]
+        [StringLength(50, ErrorMessage = "value must be 50 character or less.")]
+        public string LastName { get; set; }
+        public string ResumeFileName { get; set; }
+        public string PhotoFileName { get; set; }
+    }
+    [MetadataType(typeof(UserDetailMetaData))]
+    public partial class UserDetail { }
+}
