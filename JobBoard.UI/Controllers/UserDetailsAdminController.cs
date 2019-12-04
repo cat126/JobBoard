54f18be413a47a39d8316f8f41c386f9d8470cc5
cat126 <cat1265@gmail.com>
2019-12-04 09:27:38 -0600

    Generated UserDetailsAdmin Controller

---
+++ JobBoard.UI/Controllers/UserDetailsAdminController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using JobBoard.DataLayer;
+
+namespace JobBoard.UI.Controllers
+{
+    public class UserDetailsAdminController : Controller
+    {
+        private JobBoardEntities db = new JobBoardEntities();
+
+        // GET: UserDetailsAdmin
+        public ActionResult Index()
+        {
+            var userDetails = db.UserDetails.Include(u => u.AspNetUser);
+            return View(userDetails.ToList());
+        }
+
+        // GET: UserDetailsAdmin/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserDetail userDetail = db.UserDetails.Find(id);
+            if (userDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userDetail);
+        }
+
+        // GET: UserDetailsAdmin/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: UserDetailsAdmin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,FirstName,LastName,ResumeFileName,PhotoFileName")] UserDetail userDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserDetails.Add(userDetail);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", userDetail.UserID);
+            return View(userDetail);
+        }
+
+        // GET: UserDetailsAdmin/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserDetail userDetail = db.UserDetails.Find(id);
+            if (userDetail == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", userDetail.UserID);
+            return View(userDetail);
+        }
+
+        // POST: UserDetailsAdmin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserID,FirstName,LastName,ResumeFileName,PhotoFileName")] UserDetail userDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userDetail).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", userDetail.UserID);
+            return View(userDetail);
+        }
+
+        // GET: UserDetailsAdmin/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserDetail userDetail = db.UserDetails.Find(id);
+            if (userDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userDetail);
+        }
+
+        // POST: UserDetailsAdmin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            UserDetail userDetail = db.UserDetails.Find(id);
+            db.UserDetails.Remove(userDetail);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
