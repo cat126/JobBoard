2b4ebda6a262b79ef24886c094b99407293b6224
cat126 <cat1265@gmail.com>
2019-12-05 14:09:58 -0600

    Updated categories list on home page

--- JobBoard.UI/Controllers/HomeController.cs
+++ JobBoard.UI/Controllers/HomeController.cs
@@ -1,13 +1,19 @@
-﻿using System.Web.Mvc;
+﻿using JobBoard.DataLayer;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace JobBoard.UI.Controllers
 {
     public class HomeController : Controller
     {
+        private JobBoardEntities db = new JobBoardEntities();
+
+
         [HttpGet]
         public ActionResult Index()
         {
             ViewBag.displayHeader = false;
+            ViewBag.Categories = db.Categories.ToList();
             return View();
         }
 
