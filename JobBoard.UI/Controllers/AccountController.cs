94d500a7ffe442e4b83cfa97b5c67c2617337464
cat126 <cat1265@gmail.com>
2019-12-06 15:36:55 -0600

    All users are now added to the Employees roll when they register

--- JobBoard.UI/Controllers/AccountController.cs
+++ JobBoard.UI/Controllers/AccountController.cs
@@ -167,6 +167,7 @@ namespace JobBoard.UI.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    UserManager.AddToRole(user.Id, "Employees");
                     //var code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     //var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     //await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking this link: <a href=\"" + callbackUrl + "\">link</a>");
