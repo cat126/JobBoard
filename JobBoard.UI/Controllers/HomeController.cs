833ec632012992fa199fb838e216f9c8ee6acb59
cat126 <cat1265@gmail.com>
2019-12-06 14:12:10 -0600

    Added Contact Fourm

--- JobBoard.UI/Controllers/HomeController.cs
+++ JobBoard.UI/Controllers/HomeController.cs
@@ -1,5 +1,12 @@
 ﻿using JobBoard.DataLayer;
+using JobBoard.UI.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web.Mvc;
 
 namespace JobBoard.UI.Controllers
@@ -22,6 +29,7 @@ namespace JobBoard.UI.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your app description page.";
+            ViewBag.Message = ConfigurationManager.AppSettings.Get("MyKey");
 
             return View();
         }
@@ -29,9 +37,82 @@ namespace JobBoard.UI.Controllers
         [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]//Makes sure the post is going to the same place as the get
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            //When a class has validtion attributes that should be check before attempting to process any data
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);// we put cvm here so the forum is not wiped on a failed attempted
+            }
+            //start captcha code 
+            string captchaResponse = Request.Form["g-recaptcha-response"];
+            bool botcheckpassed = IsReCaptchValid(captchaResponse);
+            if (!botcheckpassed)
+            {
+                ViewBag.Message = "<div class=\"alert alert-danger\" role=\"alert\">You failed The bot test!</div>";
+                return View(cvm);
+            }
+            //end captcha code
+
+            string message = $"You have received an email from {cvm.Name} with a subject of {cvm.Subject}." +
+                $"Please respond to {cvm.Email} with your response to the following message: <br /> {cvm.Message}";
+            MailMessage mm = new MailMessage(ConfigurationManager.AppSettings.Get("emailFrom"), ConfigurationManager.AppSettings.Get("emailTo"), "You got a message from your website", message);
+
+            //alow html formating in the email
+            mm.IsBodyHtml = true;
+
+            // The line blow is unnessary but I put it incase I want to change ther priorty latter
+            mm.Priority = MailPriority.Normal;
+            mm.ReplyToList.Add(cvm.Email);
+
+            //SmtpClient setup
+            SmtpClient client = new SmtpClient(ConfigurationManager.AppSettings.Get("emailServer"));
+            //client credentials
+
+            client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings.Get("emailFrom"), ConfigurationManager.AppSettings.Get("emailPassword"));
+
+            //we need to handel the case were the mail server is not working
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = $"We are sorry. There was a error and your message was not sent. Please try again later Error:<br />{ex.StackTrace}";
+                return View(cvm);
+            }
+
+            return View("EmailConfirmation", cvm);
+
+        }
+        public bool IsReCaptchValid(string captchaResponse)
+        {
+            //https://www.c-sharpcorner.com/article/integration-of-google-recaptcha-in-websites/
+            //https://www.google.com/recaptcha/admin/site/347889823
+            //https://developers.google.com/recaptcha/intro
+            var result = false;
+            //var captchaResponse = Request.Form["g-recaptcha-response"];
+            var secretKey = ConfigurationManager.AppSettings["googleCaptchaSecretKey"];
+            var apiUrl = "https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}";
+            var requestUri = string.Format(apiUrl, secretKey, captchaResponse);
+            var request = (HttpWebRequest)WebRequest.Create(requestUri);
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (StreamReader stream = new StreamReader(response.GetResponseStream()))
+                {
+                    JObject jResponse = JObject.Parse(stream.ReadToEnd());
+                    var isSuccess = jResponse.Value<bool>("success");
+                    result = (isSuccess) ? true : false;
+                }
+            }
+            return result;
         }
     }
 }
---
+++ JobBoard.UI/Models/ContactViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+//We added the above line 
+namespace JobBoard.UI.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage ="* Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "* Email is required")]
+        //[RegularExpression("the expression", ErrorMessage ="Provide proper email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "* Message is required")]
+        [UIHint("MultilineText")]//makes a big text area 
+        public string Message { get; set; }
+
+
+    }
+}
