5e11330609896c5c195e11fec91daca1aa8460e4
cat126 <cat1265@gmail.com>
2019-12-11 15:09:13 -0600

    Added My Applications controller and index

---
+++ JobBoard.UI/Controllers/MyApplicationsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using JobBoard.DataLayer;
+using Microsoft.AspNet.Identity;
+
+namespace JobBoard.UI.Controllers
+{
+    [Authorize]
+    public class MyApplicationsController : Controller
+    {
+        private JobBoardEntities db = new JobBoardEntities();
+
+        // GET: MyApplications
+        public ActionResult Index()
+        {
+            string userID = User.Identity.GetUserId();
+            var applications = db.Applications.Include(a => a.ApplicationStatu).Include(a => a.OpenPosition).Include(a => a.UserDetail);
+            applications=applications.Where(x=>x.UserID== userID);
+            return View(applications.ToList());
+        }
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
